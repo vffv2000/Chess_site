1d9848572c74e9931cb158f4d9d3213daf0bf38b
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-03-30 20:33:21 +0200

    Добавлен класс RegisterUser

--- chess/views.py
+++ chess/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
+from .forms import RegisterUserForm
 from .utils import *
 
 
@@ -100,3 +103,14 @@ class ChessCategory(DataMixin, ListView):
 #         'cat_selected': cat[0].id,
 #     }
 #     return render(request, 'chess/index.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'chess/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
