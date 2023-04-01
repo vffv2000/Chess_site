4f2a9b00ab3ecab2704106e42216b52616c9290e
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-04-01 20:11:11 +0200

    Создан класс для апи

--- chess/views.py
+++ chess/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth import logout, login
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
-
+from rest_framework import generics
 from .forms import RegisterUserForm, LoginUserForm
+from .serializers import MastersSerializer
 from .utils import *
 
 
@@ -23,18 +22,6 @@ class ChessHome(DataMixin, ListView):
 
     def get_queryset(self):  # чтобы отображать не все статьи а только которые опубликованы
         return Masters.objects.filter(is_published=True)
-
-# def index(request):
-#     posts=Masters.objects.all()
-#     cats=Category.objects.all()
-#     context={
-#         'posts': posts,
-#         'cats':cats,
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'cat_selected':0,
-#     }
-#     return render(request,'chess/index.html',context=context)
 
 
 def scorelist(request):
@@ -63,17 +50,6 @@ class ShowPost(DataMixin, DetailView):
         c_def = self.get_user_context(title=context['post'])
         return context | c_def
 
-# def show_post(request,post_slug):
-#     post = get_object_or_404(Masters, slug=post_slug)
-#
-#     context={
-#         'post': post,
-#         'title': post.title,
-#         'menu': menu,
-#         'cat_selected': post.cat_id,
-#     }
-#     return render(request,'chess/post.html', context=context)
-
 
 class ChessCategory(DataMixin, ListView):
     model = Masters
@@ -89,19 +65,6 @@ class ChessCategory(DataMixin, ListView):
         c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
                                       cat_selected=context['posts'][0].cat_id)
         return context | c_def
-
-# def show_category(request,cat_slug):
-#     cat = Category.objects.filter(slug=cat_slug)
-#     posts = Masters.objects.filter(cat_id=cat[0].id)
-#     if len(posts) == 0:
-#         raise Http404()
-#     context = {
-#         'posts': posts,
-#         'title': 'Отображение по рубрикам',
-#         'menu': menu,
-#         'cat_selected': cat[0].id,
-#     }
-#     return render(request, 'chess/index.html', context=context)
 
 
 class RegisterUser(DataMixin, CreateView):
@@ -136,3 +99,8 @@ class LoginUser(DataMixin, LoginView):
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+
+class ChessAPIView(generics.ListAPIView):
+    queryset = Masters.objects.all()
+    serializer_class = MastersSerializer
