28d94f597b1fb1b1d80e73b33b446dd57d3a9202
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-04-02 15:54:43 +0200

    Создан Сериализатор для работы с моделями

--- chess/serializers.py
+++ chess/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
 from .models import *
 
 
-class MastersSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Masters
-        fields = ('title', 'cat_id')
+class MastersSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
