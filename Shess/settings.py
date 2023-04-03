95840642489a848703b6864d3b79207e1d67ec73
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-04-03 17:48:40 +0200

    Глобание настройки фремворка

--- Shess/settings.py
+++ Shess/settings.py
@@ -138,6 +138,10 @@ STATICFILES_DIRS = []
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',  # браузерное апи
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
     ]
 }
