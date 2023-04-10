b99b525e84968e689fef24c83cec4432570be3cf
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-04-10 15:02:46 +0200

    Совершены мелкие правки

--- chess/admin.py
+++ chess/admin.py
@@ -6,7 +6,7 @@ from .models import *
 class MastersAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
-
+    search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
